95701806d503710950c9202ee3b65b120bde49e7
Fitsum Alemu <FAlemu@hinda.com>
2024-10-24 15:29:24 +0300

    feat: add more color

--- src/services/productService.js
+++ src/services/productService.js
@@ -5,6 +5,6 @@ export const generateProduct = () => ({
   description: faker.commerce.productDescription(),
   price: parseFloat(faker.commerce.price()),
   images: Array.from({ length: 5 }, () => faker.image.url({ width: 400, height: 500, category: 'fashion' })),
-  colors: Array.from({ length: 2 }, () => faker.color.rgb({ format: 'css' })),
+  colors: Array.from({ length: 4 }, () => faker.color.rgb({ format: 'css' })),
   sizes: ['XS', 'S', 'M', 'L', 'XL'],
 });
